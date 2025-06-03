870543596fd1821576bcb9c13df3c3e39872e8f0
Naga raju <113161518+NagarajuBathina@users.noreply.github.com>
2025-06-03 10:38:15 +0530

    Add password attribute to employee data retrieval

--- controller/admin.js
+++ controller/admin.js
@@ -6,7 +6,18 @@ const allEmployeesData = async (req, res) => {
     const { Employee } = await connectTodb();
     const allData = await Employee.findAll({
       order: [["addedon", "DESC"]],
-      attributes: ["slno", "name", "father_name", "phone", "status", "role", "pincode", "district", "state"],
+      attributes: [
+        "slno",
+        "name",
+        "father_name",
+        "phone",
+        "status",
+        "role",
+        "pincode",
+        "district",
+        "state",
+        "password",
+      ],
     });
 
     if (!allData || allData.length === 0) {
@@ -30,7 +41,18 @@ const allRoleWiseEmployeesData = async (req, res) => {
     const allData = await Employee.findAll({
       where: { role: req.params.role },
       order: [["addedon", "DESC"]],
-      attributes: ["slno", "name", "father_name", "phone", "status", "role", "pincode", "district", "state"],
+      attributes: [
+        "slno",
+        "name",
+        "father_name",
+        "phone",
+        "status",
+        "role",
+        "pincode",
+        "district",
+        "state",
+        "password",
+      ],
     });
 
     if (!allData || allData.length === 0) {
