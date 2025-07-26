1ca3fef2855d6a6ef77837316c047b23802587de
Naga raju <113161518+NagarajuBathina@users.noreply.github.com>
2025-07-26 15:58:01 +0530

    changed port2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const employeeBankDetailsRoute = require("./routes/bank_details_route");
 
 const app = express();
 
-const PORT = process.env.PORT || 1435;
+const PORT = 1431;
 
 //middlewares
 app.use(express.json());
