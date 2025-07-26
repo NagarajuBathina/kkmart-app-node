c882ae196867d0fefcebb1460d7b6e1877174ab5
Naga raju <113161518+NagarajuBathina@users.noreply.github.com>
2025-07-26 15:52:35 +0530

    last commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const employeeBankDetailsRoute = require("./routes/bank_details_route");
 
 const app = express();
 
-const PORT = 1431;
+const PORT = process.env.PORT || 1431;
 
 //middlewares
 app.use(express.json());
